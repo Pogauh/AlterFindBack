e30d5a46f208a8cb412553ea62e7c349d097bb25
Pogauh <ultrabaga@hotmaail.com>
2024-11-28 18:48:06 +0100

    test

---
+++ src/main/java/AlterFindBack/AlterFindBackApplication.java
@@ -0,0 +1,13 @@
+package AlterFindBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlterFindBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlterFindBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/AlterFindBack/AlterFindBackApplicationTests.java
@@ -0,0 +1,13 @@
+package AlterFindBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlterFindBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
